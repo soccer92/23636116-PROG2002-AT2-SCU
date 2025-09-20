7fa5ab306e13cc20f3c7d745d4df91978e65823c
Tom Carpenter <carpth@MacBookPro.lan>
2025-09-20 14:13:27 +1000

    Added path module to assist in file and directory paths.

--- api/server.js
+++ api/server.js
@@ -5,10 +5,13 @@
 */
 
 var express = require('express');
+var path = require('path')
+
 var routerEvents = require('./routes/events'); // Events URL mapping.
 var routerCategories = require('./routes/categories'); // Categories URL mapping.
 
 var app = express();
+app.use(express.static(path.join(__dirname, '../web'))); // Serve static files from the 'web' directory.
 app.use('/api/events', routerEvents); // Map the events route.
 app.use('/api/categories', routerCategories); // Map the categories route.
 
